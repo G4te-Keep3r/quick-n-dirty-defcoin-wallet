c573172a98e5f81267a39bde280e5f7a0b34b431
G4te-Keep3r <15348093+G4te-Keep3r@users.noreply.github.com>
2021-05-06 03:41:27 -0500

    issue seems to be in defining datadir

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y build-
 
 wget https://static.coindroids.com/defcoin-bootstrap.dat.tgz
 tar -zxf defcoin-bootstrap.dat.tgz
-mkdir data
-mv bootstrap.dat data/bootstrap.dat
+#mkdir data
+#mv bootstrap.dat data/bootstrap.dat
 rm -rf defcoin-bootstrap.dat.tgz
 echo "bootstrap downloaded"
 
@@ -19,6 +19,8 @@ echo "bin added to path, starting wallet"
 
 defcoind -daemon
 #only waits 1m on docker droid, but this application needed a longer wait
-echo 'Defcoin Node initiating while we wait 5m' #5m prob way too long
-sleep 5m
+### idr why it needed longer, but since defcoin-cli is working right after it launches...im gonna set it to 2m for now
+
+echo 'Defcoin Node initiating while we wait 2m' #5m prob way too long
+sleep 2m
 echo 'and now to watch import and sync'
